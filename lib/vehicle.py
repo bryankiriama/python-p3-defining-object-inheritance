54a9a597a53caf7d84eddf9819b9e22d7db07426
bryankiriama <bryankiriama@gmail.com>
2025-11-17 19:45:13 +0300

    defined object inheritance in Python

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,11 @@
 class Vehicle:
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+        def go(self):
+            return "vroom vroom"
+        def fill_gas_tank(self):
+            return "filling up!"
+        
     pass
